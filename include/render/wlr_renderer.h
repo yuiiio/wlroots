85e2b662f195bb2999cb906d691f4ab8580b20dd
Guido Günther <agx@sigxcpu.org>
2024-11-11 23:29:45 +0100

    renderer: Drop mention of wlr_renderer_begin_with_buffer
    It got removed in 3ed1268f ("render: Nuke old read pixels API")
    
    Signed-off-by: Guido Günther <agx@sigxcpu.org>

--- include/render/wlr_renderer.h
+++ include/render/wlr_renderer.h
@@ -9,7 +9,7 @@
 struct wlr_renderer *renderer_autocreate_with_drm_fd(int drm_fd);
 /**
  * Get the supported render formats. Buffers allocated with a format from this
- * list may be attached via wlr_renderer_begin_with_buffer.
+ * list may be used with wlr_renderer_begin_buffer_pass().
  */
 const struct wlr_drm_format_set *wlr_renderer_get_render_formats(
 	struct wlr_renderer *renderer);
