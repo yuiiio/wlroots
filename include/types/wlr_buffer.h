yuiiio/wlroots

commit 1ee3ed43102d8a469d3da90f9d64d39431ce4dbe

buffer: Make wlr_buffer_is_opaque public
It's useful for compositors.

include/types/wlr_buffer.h
@@ -57,7 +57,7 @@ bool dmabuf_buffer_drop(struct wlr_dmabuf_buffer *buffer);
  * reverse is not true: false may be returned in cases where the buffer is fully
  * opaque.
  */
-bool buffer_is_opaque(struct wlr_buffer *buffer);
+bool wlr_buffer_is_opaque(struct wlr_buffer *buffer);
 
 /**
  * Creates a struct wlr_client_buffer from a given struct wlr_buffer by creating

types/buffer/buffer.c
@@ -107,7 +107,7 @@ bool wlr_buffer_get_shm(struct wlr_buffer *buffer,
 	return buffer->impl->get_shm(buffer, attribs);
 }
 
-bool buffer_is_opaque(struct wlr_buffer *buffer) {
+bool wlr_buffer_is_opaque(struct wlr_buffer *buffer) {
 	void *data;
 	uint32_t format;
 	size_t stride;

types/scene/wlr_scene.c
@@ -781,7 +781,7 @@ static void scene_buffer_set_buffer(struct wlr_scene_buffer *scene_buffer,
 	scene_buffer->buffer = wlr_buffer_lock(buffer);
 	scene_buffer->buffer_width = buffer->width;
 	scene_buffer->buffer_height = buffer->height;
-	scene_buffer->buffer_is_opaque = buffer_is_opaque(buffer);
+	scene_buffer->buffer_is_opaque = wlr_buffer_is_opaque(buffer);
 
 	scene_buffer->buffer_release.notify = scene_buffer_handle_buffer_release;
 	wl_signal_add(&buffer->events.release, &scene_buffer->buffer_release);

types/wlr_compositor.c
@@ -415,7 +415,7 @@ static void surface_apply_damage(struct wlr_surface *surface) {
 		return;
 	}
 
-	surface->opaque = buffer_is_opaque(surface->current.buffer);
+	surface->opaque = wlr_buffer_is_opaque(surface->current.buffer);
 
 	if (surface->buffer != NULL) {
 		if (wlr_client_buffer_apply_damage(surface->buffer,

commit fa97f7f1f0b27b6dbcf4636b892a6bb52a93d1e4

buffer: Move wlr_buffer_is_opaque to public header
Fixes: 1ee3ed43 ("buffer: Make wlr_buffer_is_opaque public")

Signed-off-by: Guido Günther <agx@sigxcpu.org>

include/types/wlr_buffer.h
@@ -51,15 +51,6 @@ struct wlr_dmabuf_buffer *dmabuf_buffer_create(
 bool dmabuf_buffer_drop(struct wlr_dmabuf_buffer *buffer);
 
 /**
- * Check whether a buffer is fully opaque.
- *
- * When true is returned, the buffer is guaranteed to be fully opaque, but the
- * reverse is not true: false may be returned in cases where the buffer is fully
- * opaque.
- */
-bool wlr_buffer_is_opaque(struct wlr_buffer *buffer);
-
-/**
  * Creates a struct wlr_client_buffer from a given struct wlr_buffer by creating
  * a texture from it, and copying its struct wl_resource.
  */

include/wlr/types/wlr_buffer.h
@@ -110,6 +110,15 @@ bool wlr_buffer_get_shm(struct wlr_buffer *buffer,
 struct wlr_buffer *wlr_buffer_try_from_resource(struct wl_resource *resource);
 
 /**
+ * Check whether a buffer is fully opaque.
+ *
+ * When true is returned, the buffer is guaranteed to be fully opaque, but the
+ * reverse is not true: false may be returned in cases where the buffer is fully
+ * opaque.
+ */
+bool wlr_buffer_is_opaque(struct wlr_buffer *buffer);
+
+/**
  * Buffer data pointer access flags.
  */
 enum wlr_buffer_data_ptr_access_flag {